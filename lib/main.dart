bc622dedf5066e3dcee543bd453388171deb3151
Yannick Schulz <yschulz854@gmail.com>
2022-09-09 17:05:07 +0200

    started integration of audio stream

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'dart:async';
-
+import 'dart:math';
 import 'package:flutter/material.dart';
-
+import 'package:flutter/services.dart';
+import 'package:mic_stream/mic_stream.dart';
 
 void main(List<String> args) {
   runApp(PopWatch());
@@ -28,59 +29,124 @@ class PopWatchHome extends StatefulWidget {
 
 class _PopWatchHomeState extends State<PopWatchHome> {
 
-  int milliseconds = 0;
-  int seconds = 0;
-  int minutes = 0;
+  int _milliseconds = 0;
+  int _seconds = 0;
+  int _minutes = 0;
   
-  String digitMilliseconds = '00';
-  String digitSeconds = '00';
-  String digitMinutes = '00';
+  String _digitMilliseconds = '00';
+  String _digitSeconds = '00';
+  String _digitMinutes = '00';
 
-  Timer? timer;
-  bool started = false;
-  List logs = [];
-  List textController = [];
+  Timer? _timer;
+  bool _started = false;
+  List _logs = [];
+  List _textController = [];
 
   bool _armed = false;
+  String _armedText = 'Arm';
+
+  Stream? _stream;
+  late StreamSubscription _listener;
+  List<int>? _currentSamples = [];
+  List<int> _visibleSamples = [];
+  int? _localMax;
+  int? _localMin;
+  bool _isRecording = false;
+  late bool _isActive = false;
+  late int _bytesPerSample;
+  late int _samplesPerSecond;
+
+  Future<bool> _startListening() async {
+    if(_isRecording) return false;
+
+    MicStream.shouldRequestPermission(true);
+
+    _stream = await MicStream.microphone(
+      audioSource: AudioSource.DEFAULT,
+      sampleRate: 44100,
+      channelConfig: ChannelConfig.CHANNEL_IN_MONO,
+      audioFormat: AudioFormat.ENCODING_PCM_16BIT,
+    );
+    setState(() {
+      _isRecording = true;
+    });
+    _visibleSamples = [];
+    _listener = _stream!.listen(_calculateIntensitySamples);
+    return true;
+  }
+
+  void _calculateIntensitySamples(samples) {
+    _currentSamples ??= [];
+    int currentSample = 0;
+    eachWithIndex(samples, (i, int sample) {
+      currentSample += sample;
+      if ((i % _bytesPerSample) == _bytesPerSample-1) {
+        _currentSamples!.add(currentSample);
+        currentSample = 0;
+      }
+    });
+
+    if (_currentSamples!.length >= _samplesPerSecond/10) {
+      _visibleSamples.add(_currentSamples!.map((i) => i).toList().reduce((a, b) => a+b));
+      _localMax ??= _visibleSamples.last;
+      _localMin ??= _visibleSamples.last;
+      _localMax = max(_localMax!, _visibleSamples.last);
+      _localMin = min(_localMin!, _visibleSamples.last);
+      _currentSamples = [];
+      setState(() {  });
+    }
+  }
+
+  bool _stopListening() {
+    if(!_isRecording) return false;
+    _listener.cancel();
+
+    setState(() {
+      _isRecording = false;
+      _currentSamples = null;
+    });
+    return true;
+  }
+
   double _sensitivity = 100.0;
 
   void stop() {
-    timer!.cancel();
+    _timer!.cancel();
     setState(() {
-      started = false;
+      _started = false;
     });
   }
 
   void reset() {
     logCycle();
-    timer!.cancel();
+    _timer!.cancel();
     setState(() {
-      milliseconds = 0;
-      seconds = 0;
-      minutes = 0;
+      _milliseconds = 0;
+      _seconds = 0;
+      _minutes = 0;
 
-      digitMilliseconds = '00';
-      digitSeconds = '00';
-      digitMinutes = '00';
+      _digitMilliseconds = '00';
+      _digitSeconds = '00';
+      _digitMinutes = '00';
 
-      started = false;
+      _started = false;
     });
   }
 
   void logCycle() {
-    String cycle = "$digitMinutes:$digitSeconds:$digitMilliseconds";
+    String cycle = "$_digitMinutes:$_digitSeconds:$_digitMilliseconds";
     setState(() {
-      logs.add(cycle);
-      textController.add(TextEditingController());
+      _logs.add(cycle);
+      _textController.add(TextEditingController());
     });
   }
 
   void start() {
-    started = true;
-    timer = Timer.periodic(Duration(milliseconds: 10), (timer) {
-      int localMilliseconds = milliseconds + 10;
-      int localSeconds = seconds;
-      int localMinutes = minutes;
+    _started = true;
+    _timer = Timer.periodic(Duration(milliseconds: 10), (_timer) {
+      int localMilliseconds =_milliseconds + 10;
+      int localSeconds = _seconds;
+      int localMinutes = _minutes;
 
       if(localMilliseconds > 990) {
         if(localSeconds > 58) {
@@ -93,36 +159,28 @@ class _PopWatchHomeState extends State<PopWatchHome> {
       }
 
       setState(() {
-        milliseconds = localMilliseconds;
-        seconds = localSeconds;
-        minutes = localMinutes;
+        _milliseconds = localMilliseconds;
+        _seconds = localSeconds;
+        _minutes = localMinutes;
 
-        // if(milliseconds >= 10) {
-        //   if(milliseconds >= 100) {
-        //     digitMilliseconds = "$milliseconds";
-        //   }
-        //   else {
-        //     digitMilliseconds = "0$milliseconds";
-        //   }
-        // }
-        // else {
-        //   digitMilliseconds = "00$milliseconds";
-        // }
-        digitMilliseconds = (milliseconds ~/ 10 >= 10) ?"${milliseconds ~/ 10}":"0${milliseconds ~/ 10}";
-        digitSeconds = (seconds >= 10) ?"$seconds":"0$seconds";
-        digitMinutes = (minutes >= 10) ?"$minutes":"0$minutes";
+        _digitMilliseconds = (_milliseconds ~/ 10 >= 10) ?"${_milliseconds ~/ 10}":"0${_milliseconds ~/ 10}";
+        _digitSeconds = (_seconds >= 10) ?"$_seconds":"0$_seconds";
+        _digitMinutes = (_minutes >= 10) ?"$_minutes":"0$_minutes";
           
       });
      });
   }
 
-  void arming() {
-    if(_armed) {
-      _armed = false;
-    }
-    else {
-      _armed = true;
-    }
+  void arming(dummy) {
+    setState(() {
+      if(_armed) {
+        _armed = false;
+        _armedText = 'Arm';
+      } else {
+        _armed = true;
+        _armedText = 'Armed!';
+      }      
+    });
   }
 
   void setSensitivity(sensitivity) {
@@ -146,13 +204,17 @@ class _PopWatchHomeState extends State<PopWatchHome> {
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Expanded(
-                    child: RawMaterialButton(
+                  Center(child: ToggleButtons(
+                      constraints: BoxConstraints.expand(width: 110.0, height: 40.0 ),
+                      children: [Text(_armedText)],
+                      isSelected: [_armed],
                       onPressed: arming,
-                      shape: const StadiumBorder(side: BorderSide(color: Color.fromARGB(255, 3, 70, 9))),
-                      child: const Text('Arm',
-                        style: TextStyle(color: Color.fromARGB(255, 255, 255, 255))
-                      ),
+                      selectedBorderColor: Color.fromARGB(255, 255, 0, 0),
+                      borderRadius: BorderRadius.circular(8.0),
+                      borderColor: Color.fromARGB(255, 0, 255, 0),
+                      color: Color.fromARGB(255, 255, 255, 255),
+                      selectedColor: Color.fromARGB(255, 255, 255, 255),
+                      fillColor: Color.fromARGB(75, 255, 0, 0),
                     ),
                   ),
                   const SizedBox(width: 10.0,),
@@ -168,7 +230,7 @@ class _PopWatchHomeState extends State<PopWatchHome> {
                   const SizedBox(width: 10.0,),
                   Expanded(
                     child: RawMaterialButton(
-                      onPressed: () { (!started) ? start() : stop();},
+                      onPressed: () { (!_started) ? start() : stop();},
                       shape: const StadiumBorder(side: BorderSide(color: Color.fromARGB(255, 48, 75, 165))),
                       child: const Text('Start',
                         style: TextStyle(color:  Color.fromARGB(255, 255, 255, 255))
@@ -184,7 +246,7 @@ class _PopWatchHomeState extends State<PopWatchHome> {
                   borderRadius: BorderRadius.circular(6.0)
                 ),
                 child: Center(
-                  child: Text('$digitMinutes:$digitSeconds.$digitMilliseconds',
+                  child: Text('$_digitMinutes:$_digitSeconds.$_digitMilliseconds',
                     style: TextStyle(
                       color: Color.fromARGB(255, 255, 255, 255),
                       fontSize: 75.0,
@@ -239,7 +301,7 @@ class _PopWatchHomeState extends State<PopWatchHome> {
                   borderRadius: BorderRadius.circular(6.0)
                 ),
                 child: ListView.builder(
-                  itemCount: logs.length,
+                  itemCount: _logs.length,
                   itemBuilder: (context, index) {
                     return Padding(
                       padding: const EdgeInsets.fromLTRB(16.0, 1.0, 5.0, 1.0),
@@ -250,7 +312,7 @@ class _PopWatchHomeState extends State<PopWatchHome> {
                           Expanded(
                             child: SizedBox(height: 25.0, 
                               child: TextField(
-                                controller: textController[index],
+                                controller: _textController[index],
                                 style: TextStyle(
                                   color: Colors.white
                                 ),
@@ -268,11 +330,11 @@ class _PopWatchHomeState extends State<PopWatchHome> {
                             ),
                           ),
                           SizedBox(width: 10.0),
-                          Text("${logs[index]}", style: TextStyle(color: Color.fromARGB(255, 255, 255, 255))),
+                          Text("${_logs[index]}", style: TextStyle(color: Color.fromARGB(255, 255, 255, 255))),
                           IconButton(onPressed: () { setState(() {
-                            logs.removeAt(index);
-                            textController[index].clear();
-                            textController.removeAt(index);
+                            _logs.removeAt(index);
+                            _textController[index].clear();
+                            _textController.removeAt(index);
                           });}, icon: Icon(Icons.cancel)),
                         ],
                       ),
@@ -288,6 +350,17 @@ class _PopWatchHomeState extends State<PopWatchHome> {
   }
 }
 
+Iterable<T> eachWithIndex<E, T>(
+    Iterable<T> items, E Function(int index, T item) f) {
+  var index = 0;
+
+  for (final item in items) {
+    f(index, item);
+    index = index + 1;
+  }
+
+  return items;
+}
 
 // import 'package:flutter/material.dart';
 
